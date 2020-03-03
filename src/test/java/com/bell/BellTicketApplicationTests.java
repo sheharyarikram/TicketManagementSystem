6e3a921b7ce2ca0dca55bcf5243c050ea888e266
Sheharyar Ikram <sheharyar.ikram@mail.mcgill.ca>
2020-03-03 01:16:45 -0500

    Add files via upload

---
+++ src/test/java/com/bell/BellTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BellTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
